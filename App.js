c2131f2a1422f9de4b4368392e09bf64204bc8b0
Arsalan Jamal <arsln.jamal71@gmail.com>
2023-06-01 17:18:59 +0500

    "first commit"

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect,useRef } from 'react';
-import { StyleSheet, TouchableOpacity, Linking,Text, Animated, View, Alert,Image,Dimensions } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, TouchableOpacity, Linking, Text, Animated, View, Alert, Image, Dimensions } from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
 import { ReactPropTypes } from 'react';
@@ -12,16 +12,16 @@ export default function App() {
   const handleToggle = () => {
     setIsOn(!isOn);
   };
-  const onSuccess = (e) => {
-    Linking.openURL(e.data).catch(err =>
-        console.error('An error occurred', err)
+  const achived = (a) => {
+    Linking.openURL(a.data).catch(err =>
+      console.error('An error occurred', err)
     );
-};
+  };
 
   Animated.loop(
     Animated.parallel([
       Animated.timing(scaleValue, {
-        toValue: 1,
+        toValue: 1.2,
         duration: 1000,
         useNativeDriver: false,
       }),
@@ -35,117 +35,91 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-    <View style={styles.imagesCon}>
-    <Image source={require('./images/arrow.png')} style={styles.backImage} />
-    <Image source={require('./images/logo.png')} style={styles.logoImage}     />
-    <Image source={require('./images/london.png')} style={styles.londonImage} />
-    </View>
-    <QRCodeScanner
-    style={styles.QRCodeScanner}
-    onRead={onSuccess}
-    flashMode={isOn ? RNCamera.Constants.FlashMode.torch : RNCamera.Constants.FlashMode.off}
-    reactivate={true}
-    reactivateTimeout={5000}
-  />
-  <Animated.View
+      <View style={styles.imagesCon}>
+        <Image source={require('./images/arrow.png')} style={styles.backImage} />
+        <Image source={require('./images/logo.png')} style={styles.logoImage} />
+        <Image source={require('./images/london.png')} style={styles.londonImage} />
+      </View>
+      <QRCodeScanner
+        style={styles.QRCodeScanner}
+        onRead={achived}
+        flashMode={isOn ? RNCamera.Constants.FlashMode.torch : RNCamera.Constants.FlashMode.off}
+        reactivate={true}
+        reactivateTimeout={5000}
+      />
+      <Animated.View
         style={[
           styles.square,
           {
-            height:250,
-            width:250,
-            borderRadius:30,
-            borderStyle:"dotted",
+            height: 250,
+            width: 250,
+            borderRadius: 30,
+            borderStyle: "dotted",
             transform: [{ scale: scaleValue }],
             opacity: opacityValue,
           },
         ]}
       />
-  <View style={styles.bottomnav}>
-    <Image source={require('./images/qr.png')} style={styles.qrImage} />
-    <Image source={require('./images/HISTORY.png')} style={styles.historyImage}     />
-  <TouchableOpacity onPress={handleToggle}>
+      <View style={styles.bottomnav}>
+        <Image source={require('./images/qr.png')} style={styles.qrImage} />
+        <Image source={require('./images/HISTORY.png')} style={styles.historyImage} />
+        <TouchableOpacity onPress={handleToggle}>
           <View style={styles.tourchImage}>
             <Image style={styles.image} resizeMode='contain' source={require('./images/tourch.png')} />
           </View>
         </TouchableOpacity>
+      </View>
     </View>
-    
-  </View>
   )
 }
 
 
 const styles = StyleSheet.create({
-container:{
-flex:1
-},
+  container: {
+    flex: 1
+  },
   logoImage: {
-  left:"160%"
+    left: "160%"
   },
-  backImage:{
-    top:5,
-    left:2,
+  backImage: {
+    top: 5,
+    left: 2,
   },
-  londonImage:{
-    top:3,
+  londonImage: {
+    top: 3,
   },
-  QRCodeScanner:{
- height:Dimensions.get('window').height,
- width:Dimensions.get('window').width,
+  QRCodeScanner: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
   },
-  imagesCon:{
-    backgroundColor:"grey",
-    padding:20,
-    flexDirection:"row",
-    justifyContent:"space-between",
+  imagesCon: {
+    backgroundColor: "grey",
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  tourchImage:{
-    alignSelf:"flex-end",
-    right:"5%"
+  tourchImage: {
+    alignSelf: "flex-end",
+    right: "5%"
   },
-  bottomnav:{
-    backgroundColor:"grey",
-    padding:20,
-    flexDirection:"row",
-    justifyContent:"space-between",
+  bottomnav: {
+    backgroundColor: "grey",
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  qrImage:{
-    left:10,
+  qrImage: {
+    left: 10,
 
-    },
-  historyImage:{
-    top:6,
+  },
+  historyImage: {
+    top: 6,
   },
   square: {
     position: 'absolute',
-    top:"35%",
-    alignSelf:"center",
+    top: "35%",
+    alignSelf: "center",
     borderWidth: 2,
     borderColor: 'green',
   },
 });
-
-// import React from 'react';
-// import { View, Image, StyleSheet } from 'react-native';
-
-// const MyComponent = () => {
-//   return (
-//     <View style={styles.container}>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   image: {
-//     width: 200,
-//     height: 200,
-//     backgroundColor:"blue"
-//   },
-// });
-
-// export default MyComponent;
